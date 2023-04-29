ecde88501caae15328049a88a78db7141196b328
uslnck <117448466+uslnck@users.noreply.github.com>
2023-04-29 20:58:00 +0300

    1

---
+++ src/components/app/app.tsx
@@ -0,0 +1,124 @@
+//@ts-nocheck
+
+import "./app.css";
+import { Row, Col, Card } from "antd";
+import { useState, useEffect } from "react";
+
+class SearchService {
+  _baseUrl = "https://api.themoviedb.org/3/search/";
+  _apiKey = "?api_key=0181923591c91859e91691704fe87633";
+  _noAdult = "&include_adult=false";
+  _lang = "&language=en-US";
+
+  async getResource(query, searchType, page = 1) {
+    const q = "&query=" + query;
+    const s = searchType;
+    const p = "&page=" + page;
+    const res = await fetch(
+      `${this._baseUrl}${s}${this._apiKey}${this._lang}${q}${p}${this._noAdult}`
+    );
+    if (!res.ok) throw new Error("Couldn't fetch URL");
+    const body = await res.json();
+    return body;
+  }
+
+  async searchMovie(query) {
+    const res = await this.getResource(query, "movie");
+    return res.results;
+  }
+
+  async searchPeople(query) {
+    const res = await this.getResource(query, "people");
+    return res.results;
+  }
+}
+
+const ss = new SearchService();
+
+// const randomMovie = ss.searchMovie("return").then((body) => {
+//   return body[Math.floor(Math.random() * 21)];
+// });
+
+const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const randomMovie = await ss.searchMovie("return");
+        const movieData = randomMovie.map((movie) => ({
+          title: movie.original_title,
+          date: movie.release_date,
+          genres: ["Action", "Drama"],
+          description: movie.overview,
+          posterUrl: "https://example.com/movie1-poster.jpg",
+        }));
+        setMovies(movieData);
+      } catch (e) {
+        throw new Error("Couldn't fetch movies", e);
+      }
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <h1>Movies</h1>
+      <Row gutter={[0, 40]} justify="space-evenly">
+        {movies.map(({ genres, posterUrl, date, description, title }, i) => (
+          <Col span={24} md={11} lg={11} sm={5} key={i}>
+            <Card
+              className="card"
+              bodyStyle={{
+                paddingBottom: 0,
+                paddingTop: 0,
+                paddingLeft: 0,
+                paddingRight: 0,
+              }}
+            >
+              <Row gutter={[16, 16]}>
+                <Col span={8}>
+                  <img src={posterUrl} alt={title} />
+                </Col>
+                <Col
+                  span={16}
+                  style={{
+                    height: 450,
+                    paddingBottom: 20,
+                    paddingTop: 20,
+                    paddingLeft: 20,
+                    paddingRight: 40,
+                  }}
+                >
+                  <h2 className="title">{title}</h2>
+                  <p className="date">{date}</p>
+                  <p>
+                    {genres.map((genre, i) => {
+                      return (
+                        <span
+                          className="genre"
+                          key={i}
+                          style={{
+                            marginRight: 7,
+                            paddingLeft: 3,
+                            paddingRight: 3,
+                          }}
+                        >
+                          {genre}
+                        </span>
+                      );
+                    })}
+                  </p>
+
+                  <p className="description">{description}</p>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default MovieList;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+// @ts-nocheck
+
+import "./index.css";
+
+import { createRoot } from "react-dom/client";
+import App from "./components/app";
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
